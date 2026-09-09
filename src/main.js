88a78612eb897cb5e7b67c93e95a4f7a025b0f4e
SML_365 <samuel.lukac.365@gmail.com>
2026-09-08 19:04:15 -0700

    Started work on new tab page, added background, and started clock face

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-import './style.css'
+// const API_KEY = import.meta.env.VITE_NASA_API_KEY
+
+// fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).
+// then(response => response.json()).then(data => {
+//   let media;
+
+//   if(data.media_type === "image") {
+//     media = `<image src="${data.url}"/>`
+//   } else if (data.url.includes("youtube")){
+//     media = `<iframe src="${data.url}"></iframe>`
+//   } else {
+//     media = `<video src="${data.url}" controls></video>`
+//   }
+
+//    document.body.style.backgroundImage = `url(${data.url})`
+//    ;
+// })
+// .catch(err => {
+//     document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+//   });
